37a94123d3eb06090f8da892451b755c00a16b8c
estiven-lg <estivenlaferre@gmail.com>
2026-05-13 20:55:52 -0600

    ....

---
+++ frontend/src/pages/M6PrizeDistribution/main.jsx
@@ -0,0 +1,7 @@
+export default function M6PrizeDistributionMain() {
+  return (
+    <div>
+      <h1>M6 - Distribución de Premios</h1>
+    </div>
+  );
+}
---
+++ frontend/src/pages/M7AdminPanel/index.jsx
@@ -0,0 +1,3 @@
+import M7AdminPanelMain from './main';
+
+export default M7AdminPanelMain;
